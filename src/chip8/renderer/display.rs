9ee8853c21a3ffef9d915eadfde62cb7f4fd7f28
Dorian 'Renji' Peron <dodo.savenay@gmail.com>
2022-02-06 01:58:49 +0100

    Added comments in the renderer

--- src/chip8/renderer/display.rs
+++ src/chip8/renderer/display.rs
@@ -1,3 +1,12 @@
+/**
+ * Simple Structure allowing to munipulate pixels
+ * of a 64x32 screen.
+ *
+ * Because the screen is only 64 pixels large, we can
+ * use u64 types to hold all the bits for a line.
+ * This approach prevents from memory waste that the
+ * '1 char per pixel' method would have produced.
+ */
 pub struct Display {
     pub tab: [u64; 32],
 }
@@ -6,12 +15,16 @@ impl Display {
     pub fn new() -> Display {
         Display { tab: [0; 32] }
     }
+
+    /**
+     * Set all the pixels to 0
+     */
     pub fn clear(&mut self) {
-        // Set all the values to 0
         for i in self.tab.iter_mut() {
             *i = 0u64;
         }
     }
+
     pub fn draw_sprite(&mut self, x: u8, y: u8, byte: u8) {
         // Get the 'line' to update
         let mut val: u64 = self.tab[y as usize];
--- src/chip8/renderer/mod.rs
+++ src/chip8/renderer/mod.rs
@@ -1,13 +1,18 @@
 // Exported modules
 pub mod ascii_display;
+
+// Avoid verbosity
 pub use ascii_display::AsciiDisplay;
 
-// Internal use modules
-mod display;
-
+/**
+ * Define the functions a Chip8 screen renderer should have
+ */
 pub trait ChipRenderer {
     fn new() -> Self;
     fn render(&self);
     fn clear(&mut self);
     fn draw_sprite(&mut self, x: u8, y: u8, byte: u8);
 }
+
+// INTERNAL USE
+mod display;
